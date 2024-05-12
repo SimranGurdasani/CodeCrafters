f09c0cda7086912453be280ef9d208aca3488b74
simran <simrangurdasani05@gmail.com>
2024-05-12 05:54:31 +0530

    updated

---
+++ ServiceProviders/ServiceProviders.js
@@ -0,0 +1,16 @@
+function redirectToAddServiceProvider(name, experience, flexibility) {
+       
+    localStorage.setItem("name", name);
+    localStorage.setItem("experience", experience);
+    localStorage.setItem("flexibility", flexibility);
+
+
+     
+     var url = `/AddServiceProvider/addservice.html`;
+
+     
+     window.location.href = url;
+    
+ }
+
+  
